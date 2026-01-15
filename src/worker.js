ff27d466dbaa43cfbeac200979137750a459e3c9
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2026-01-15 13:25:04 -0800

    Initial commit

---
+++ src/worker.js
@@ -0,0 +1,23 @@
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+import manifestJSON from '__STATIC_CONTENT_MANIFEST';
+
+const assetManifest = JSON.parse(manifestJSON);
+
+export default {
+  async fetch(request, env, ctx) {
+    try {
+      return await getAssetFromKV(
+        {
+          request,
+          waitUntil: ctx.waitUntil.bind(ctx),
+        },
+        {
+          ASSET_NAMESPACE: env.__STATIC_CONTENT,
+          ASSET_MANIFEST: assetManifest,
+        }
+      );
+    } catch (e) {
+      return new Response('Not Found', { status: 404 });
+    }
+  },
+};
